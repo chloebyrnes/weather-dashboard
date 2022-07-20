0f1fb929807f05b4a07eb2c22bb3932527032842
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-07-19 20:27:21 -0400

    added new header

--- assests/script.js
+++ assests/script.js
@@ -10,5 +10,25 @@ var humidity = document.querySelector('.humidity');
 var uvindex = document.querySelector('.uvindex');
 
 
+//event listener
+button.addEventListener('click', function() {
+
 //fetching api
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=6e61d5c7ad393f7fd70107fe83b9f92a')
+fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={6e61d5c7ad393f7fd70107fe83b9f92a}')
+  .then(response => response.json())
+  .then(data => {
+    var nameValue = data['name'];
+    var tempValue = data['temp'];
+    var windValue = data['wind'];
+    var humidityValue = data['humidity']
+
+    name.innerHTML =nameValue;
+    temp.innerHTML =tempValue;
+    wind.innerHTML =windValue;
+    humidity.innerHTML =humidityValue;
+  })
+
+  catch.(err => alert("wrong city name"))
+
+
+})
