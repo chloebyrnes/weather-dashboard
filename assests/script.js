926b78d495125f72cb0d57fcf1db057f7d5bb1be
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-07-21 14:05:09 -0400

    added script to html and added functions to js for weather data

--- assests/script.js
+++ assests/script.js
@@ -8,27 +8,68 @@ var temp = document.querySelector('.temp');
 var wind = document.querySelector('.wind');
 var humidity = document.querySelector('.humidity');
 var uvindex = document.querySelector('.uvindex');
+var apiKey = "6e61d5c7ad393f7fd70107fe83b9f92a";
+var searchInput = "";
+var city = "";
+var currentDate = moment().format("M/D/YYYY");
 
 
 //event listener
 button.addEventListener('click', function() {
+  console.log("button was clicked")
+  searchInput = $(".inputValue").val().trim();
+  getLocation(searchInput);
+});
+
+
+
+function getLocation(searchInput){
+
 
 //fetching api
-fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={6e61d5c7ad393f7fd70107fe83b9f92a}')
-  .then(response => response.json())
-  .then(data => {
-    var nameValue = data['name'];
-    var tempValue = data['temp'];
-    var windValue = data['wind'];
-    var humidityValue = data['humidity']
+var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=1&appid=" + apiKey;
+fetch(apiUrl)
+  .then(function(response) {
+    if(response.ok){
+      response.json().then(function(data){
+        console.log(data)
+        var lattitude = data[0].lat;
+        var longitude = data[0].lon;
+        city = data[0].name;
+        var lattitudeStr = lattitude.toString();
+        var longitudeStr = longitude.toString();
+        getWeather(lattitudeStr, longitudeStr);
+      });
+    }
+  });
+};
 
-    name.innerHTML =nameValue;
-    temp.innerHTML =tempValue;
-    wind.innerHTML =windValue;
-    humidity.innerHTML =humidityValue;
-  })
+  function getWeather(lat, lon){
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + apiKey;
+    fetch (apiUrl).then(function(response){
+      if(response.ok){
+        response.json().then(function(data){
+          console.log(data)
 
-  catch.(err => alert("wrong city name"))
+          var nameElement = $(".name");
+          nameElement.text(city.toUpperCase()+ currentDate);
+          var tempElement = $(".temp");
+          tempElement.text(data.current.temp);
+          
+
+        })
+      }
+    })
+  }
 
 
-})
+   // var nameValue = data['name'];
+   // var tempValue = data['temp'];
+   // var windValue = data['wind'];
+    //var humidityValue = data['humidity']
+
+   // name.innerHTML =nameValue;
+   // temp.innerHTML =tempValue;
+   // wind.innerHTML =windValue;
+   // humidity.innerHTML =humidityValue;
+
